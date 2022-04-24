a0400246eaa833fbd1bb07e30ffd8716917d111f
claudia <claudiakuczun@gmail.com>
2022-04-23 21:01:29 -0400

    one more

--- include/Graph.h
+++ include/Graph.h
@@ -209,63 +209,6 @@ struct Graph{
 		}
 
 
-
-	// EFFICIENCY & POWER FUNCTIONS (DANI)
-		
-    // Powertrain Efficiency function
-  double npt(T& velocity){
-
-    double npt = 0.95 - pow(1.2, (-velocity + (log(0.95)/log(1.2))));
-
-    return npt;
-  }
-
-    // Total energy consumption function
-  double Energy(T& velocity){
-
-    double total_energy;
-    double dist = 2*1609; // 2 miles converted to meters
-
-    // vehicle parameters
-    double g = 9.806; // gravity
-    double Aden = 1.225; // air density at sea level in kg/m^3
-
-    double Ppara = 1300; // parasitic power (Watts)
-    double mass = 3700 * 0.453592; // Mass of car in kilograms
-
-    double A = 2.22; // frontal area in m^2
-    double Cd = 0.23; // drag coefficient
-
-    double npt = npt(velocity);
-
-    // Drag-Area Product
-    double CdA = Cd*A;
-
-
-    // Air drag force equation
-    double ADrag = ((1/(2*npt))*(CdA*Aden));
-
-    // Convert mph to m/s
-    double Vel = velocity*0.44704;
-
-    // Rolling Resistance equation
-    double roll = 0.0075 + (3.078*pow(10,-6)*pow(Vel,2));
-
-    // Air drag portion
-    double a = ADrag*pow(Vel,2)*dist;
-
-    // Rolling Resistance portion
-    double b = (mass*g)*(roll/npt)*dist;
-
-    // Power portion
-    double c = (Ppara/Vel)*dist;
-
-    // Total energy consumption (in watt-seconds)
-    total_energy = a + b + c;
-
-    return total_energy/3600000;
-  }
-
     // Dijkstra's Algorithm
   void Dijkstra( unsigned int origin, unsigned int destin ){
 
